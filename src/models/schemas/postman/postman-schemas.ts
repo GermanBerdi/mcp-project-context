be081e0393fefa0168dfa6acdac9d3074f18b25e
German Berdichevsky <gberdichevsky@naevatec.com>
2026-01-22 08:57:15 +0100

    refactor: remove unnecessary exports of item schemas in postman types

--- src/models/schemas/postman/postman-schemas.ts
+++ src/models/schemas/postman/postman-schemas.ts
@@ -2,7 +2,5 @@ export { createCollectionReqSchema as createCollectionReq } from "./create-colle
 export { createdCollectionSchema as createdCollection } from "./create-collection-res.js";
 export { createCollectionResSchema as createCollectionRes } from "./create-collection-res.js";
 export { listCollectionsReqSchema as listCollectionsReq } from "./list-collections-req.js";
-export { listCollectionItemSchema as collection } from "./list-collections-res.js";
 export { listCollectionsResSchema as listCollectionsRes } from "./list-collections-res.js";
-export { listWorkspaceItemSchema as workspace } from "./list-workspaces-res.js";
 export { listWorkspacesResSchema as listWorkspacesRes } from "./list-workspaces-res.js";
--- src/models/types/postman/postman-types.ts
+++ src/models/types/postman/postman-types.ts
@@ -4,15 +4,13 @@ import { collectionSchema } from "../../schemas/postman/collection/collection.js
 import { createCollectionReqSchema } from "../../schemas/postman/create-collection-req.js";
 import { createdCollectionSchema, createCollectionResSchema } from "../../schemas/postman/create-collection-res.js";
 import { listCollectionsReqSchema } from "../../schemas/postman/list-collections-req.js";
-import { listCollectionItemSchema, listCollectionsResSchema } from "../../schemas/postman/list-collections-res.js";
-import { listWorkspaceItemSchema, listWorkspacesResSchema } from "../../schemas/postman/list-workspaces-res.js";
+import { listCollectionsResSchema } from "../../schemas/postman/list-collections-res.js";
+import { listWorkspacesResSchema } from "../../schemas/postman/list-workspaces-res.js";
 
 export type Collection = z.infer<typeof collectionSchema>;
 export type CreateCollectionReq = z.infer<typeof createCollectionReqSchema>;
 export type CreatedCollection = z.infer<typeof createdCollectionSchema>;
 export type CreateCollectionRes = z.infer<typeof createCollectionResSchema>;
 export type ListCollectionsReq = z.infer<typeof listCollectionsReqSchema>;
-export type ListCollectionItem = z.infer<typeof listCollectionItemSchema>;
 export type ListCollectionsRes = z.infer<typeof listCollectionsResSchema>;
-export type ListWorkspaceItem = z.infer<typeof listWorkspaceItemSchema>;
 export type ListWorkspacesRes = z.infer<typeof listWorkspacesResSchema>;
